6bd1b1486ed7a27ffe0e39e12f88c4f5d6b79ce6
subhan <sub.metu@gmail.com>
2016-08-25 00:33:35 +0400

    Comments and variable changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 //DEFINING BASIC VARIABLES
 var html='';
-var quoteArray=[];
-var currentQuote;
+var quoteArray=[]; // array that holds array indices for the global quotes array
+var currentQuote; 
 var currentColor;
 var colorArray=[];
-var colorArrayRef=['#b00b00','#8B8989','#DC8787','#6B4226','#FCE6C9','#36b55c']; //reference Array WHICH DOES NOT CHANGE
+var colorArrayRef=['#b00b00','#8B8989','#DC8787','#6B4226','#FCE6C9','#36b55c']; //reference Array WHICH DOES NOT CHANGE IN CODE, It is predefined because 'completely' random colors are not preferred since some colors are very undesirable and not pleasent
+
 var myTimer;
 
 //PRINT FUNCTION TO PRINT TO THE DIV WITH ID quote-box
@@ -14,14 +15,14 @@ function print(message)
     outputDiv.innerHTML=message;
 }
 
-//GENERATES AN INDEX ARRAY ACCORDING TO THE NUMBER OF ELEMENTS IN THE quotes ARRAY
+//GENERATES AN INDEX ARRAY ACCORDING TO THE NUMBER OF ELEMENTS IN THE quotes ARRAY so we can change quotes array and everything will still work
 function GenQuoteArray(array)
 {
-    for(var i=0;i<quotes.length;i++)
+    for(var i=0;i<quotes.length;i++) // loop through according to the elements in the quotes array
     {
-        quoteArray.push(i);
+        quoteArray.push(i);//fill the quoteArray with elements 1,2,3,4 .... etc according to the elements in the quotes array
     }
-    return array;
+    
 }
 
 //GENERATES A colorArray BY GETTING HEXADECIMAL COLOR VALUES FROM colorArrayRef
@@ -36,16 +37,19 @@ function GenColorArray(array)
 //SHUFFLES AN ARRAY TO RANDOMIZE THE ELEMENTS IN IT
 function shuffle(array)
 {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    while (0 !== currentIndex) 
+    var currentIndex = array.length, 
+    tempVal, //holds a temporary array with the index of the length of the passed array
+    randIndex; //random index value got from the math.random() function
+    while (currentIndex!==0) //run the loop according to how many elements are in the passed array
     { 
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    randIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1; //subtracts the value of currentIndex by 1 in every loop
+    //switch the values of array[currentIndex] and array[randIndex]
+    tempVal = array[currentIndex];
+    array[currentIndex] = array[randIndex];
+    array[randIndex] = tempVal;
     }
-    return array;
+    
 }
 
 //INTERMEDIATE FUNCTION THAT GENERATES AN INDEX ARRAY ACCORDING TO THE NUMBER OF ELEMENTS IN THE quotes ARRAY AND THEN SHUFFLES IT
@@ -65,8 +69,8 @@ function repopulateColor()
 //GETS AN INDEX ELEMENT FROM THE quoteArray AND RETURNS THE CORRESPONDING quotes ARRAY ELEMENT OBJECT
 function getRandomQuote()
 {   
-    currentQuote=quoteArray.pop();
-    return quotes[currentQuote];
+    currentQuote=quoteArray.pop(); //takes the last element of the quoteArray and puts it into currentQuote
+    return quotes[currentQuote]; //returns the quotes array with the currentQuote element
 }
 
 //GETS A COLOR FROM THE colorArray AND THEN RETURNS IT
@@ -83,7 +87,7 @@ function getHtmlVar()
     //DEFINE THE html STRING TO PRINT TO THE PAGE
     html+='<p class="quote">'+ randQuote.quote +'</p>';
     html+='<p class="source"> '+ randQuote.source;
-    //IF THERE IS A CITATION IN QUOTE THEN PRINT IT
+//    IF THERE IS A CITATION IN QUOTE THEN PRINT IT
     if(quotes[currentQuote].citation!==''){html+='<span class="citation">'+randQuote.citation +'</span>';}
     //IF THERE IS A YEAR IN THE QUOTE THEN PRINT IT
     if(quotes[currentQuote].year!==''){html+='<span class="year">'+ randQuote.year +'</span>';} 
